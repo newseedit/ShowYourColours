1bb4d8985a62b25f534d6674f5c37f929acd09f9
Magnus Lindgren <magnus@newseed.se>
2014-10-18 18:17:18 +0200

    Ok we are starting!

---
+++ api.php
@@ -0,0 +1,42 @@
+<?php
+
+//phpinfo();
+
+/*** mysql hostname ***/
+$hostname = 'localhost';
+
+/*** mysql username ***/
+$username = 'root';
+
+/*** mysql password ***/
+$password = '';
+
+try {
+    $dbh = new PDO("mysql:host=127.0.0.1;dbname=mysql", $username, $password);
+    /*** echo a message saying we have connected ***/
+    /*
+    echo 'Connected to database';
+    $sth = $dbh->prepare("show databases");
+    $sth->execute();
+    $data = $sth->fetchAll();
+    print_r($data);
+    */
+}
+catch(PDOException $e){
+    echo $e->getMessage();
+    exit;
+}
+
+
+/**/
+
+
+$json = array();
+
+$json['test'] = 'helloworld!';
+
+if ($resource == 'hashtag'){
+	
+}
+
+print json_encode($json);
